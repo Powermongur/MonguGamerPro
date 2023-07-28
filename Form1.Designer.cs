4dedfe61d629ea894d19edcc4b3742b3c8747cf6
Powermongur <cars10@cars10.dk>
2023-07-28 20:09:21 +0200

    -

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.capsLockButton = new System.Windows.Forms.Button();
+            this.numLockButton = new System.Windows.Forms.Button();
+            this.scrollLockButton = new System.Windows.Forms.Button();
+            this.numLockLabel = new System.Windows.Forms.Label();
+            this.capsLockLabel = new System.Windows.Forms.Label();
+            this.scrollLockLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // capsLockButton
+            // 
+            this.capsLockButton.Location = new System.Drawing.Point(117, 143);
+            this.capsLockButton.Name = "capsLockButton";
+            this.capsLockButton.Size = new System.Drawing.Size(75, 23);
+            this.capsLockButton.TabIndex = 0;
+            this.capsLockButton.Text = "Caps Lock";
+            this.capsLockButton.UseVisualStyleBackColor = true;
+            this.capsLockButton.Click += new System.EventHandler(this.capsLockButton_Click);
+            // 
+            // numLockButton
+            // 
+            this.numLockButton.Location = new System.Drawing.Point(117, 97);
+            this.numLockButton.Name = "numLockButton";
+            this.numLockButton.Size = new System.Drawing.Size(75, 23);
+            this.numLockButton.TabIndex = 1;
+            this.numLockButton.Text = "Num Lock";
+            this.numLockButton.UseVisualStyleBackColor = true;
+            this.numLockButton.Click += new System.EventHandler(this.numLockButton_Click);
+            // 
+            // scrollLockButton
+            // 
+            this.scrollLockButton.Location = new System.Drawing.Point(117, 197);
+            this.scrollLockButton.Name = "scrollLockButton";
+            this.scrollLockButton.Size = new System.Drawing.Size(75, 23);
+            this.scrollLockButton.TabIndex = 2;
+            this.scrollLockButton.Text = "Scroll Lock";
+            this.scrollLockButton.UseVisualStyleBackColor = true;
+            this.scrollLockButton.Click += new System.EventHandler(this.scrolLockButton_Click);
+            // 
+            // numLockLabel
+            // 
+            this.numLockLabel.AutoSize = true;
+            this.numLockLabel.BackColor = System.Drawing.Color.Red;
+            this.numLockLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numLockLabel.ForeColor = System.Drawing.Color.White;
+            this.numLockLabel.Location = new System.Drawing.Point(198, 100);
+            this.numLockLabel.Name = "numLockLabel";
+            this.numLockLabel.Size = new System.Drawing.Size(36, 16);
+            this.numLockLabel.TabIndex = 3;
+            this.numLockLabel.Text = "OFF";
+            // 
+            // capsLockLabel
+            // 
+            this.capsLockLabel.AutoSize = true;
+            this.capsLockLabel.BackColor = System.Drawing.Color.Red;
+            this.capsLockLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.capsLockLabel.ForeColor = System.Drawing.Color.White;
+            this.capsLockLabel.Location = new System.Drawing.Point(198, 146);
+            this.capsLockLabel.Name = "capsLockLabel";
+            this.capsLockLabel.Size = new System.Drawing.Size(36, 16);
+            this.capsLockLabel.TabIndex = 4;
+            this.capsLockLabel.Text = "OFF";
+            // 
+            // scrollLockLabel
+            // 
+            this.scrollLockLabel.AutoSize = true;
+            this.scrollLockLabel.BackColor = System.Drawing.Color.Red;
+            this.scrollLockLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.scrollLockLabel.ForeColor = System.Drawing.Color.White;
+            this.scrollLockLabel.Location = new System.Drawing.Point(198, 200);
+            this.scrollLockLabel.Name = "scrollLockLabel";
+            this.scrollLockLabel.Size = new System.Drawing.Size(36, 16);
+            this.scrollLockLabel.TabIndex = 5;
+            this.scrollLockLabel.Text = "OFF";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.scrollLockLabel);
+            this.Controls.Add(this.capsLockLabel);
+            this.Controls.Add(this.numLockLabel);
+            this.Controls.Add(this.scrollLockButton);
+            this.Controls.Add(this.numLockButton);
+            this.Controls.Add(this.capsLockButton);
+            this.Name = "Form1";
+            this.Text = "Mongu Gamer Pro";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button capsLockButton;
+        private System.Windows.Forms.Button numLockButton;
+        private System.Windows.Forms.Button scrollLockButton;
+        private System.Windows.Forms.Label numLockLabel;
+        private System.Windows.Forms.Label capsLockLabel;
+        private System.Windows.Forms.Label scrollLockLabel;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,13 +8,95 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Runtime.InteropServices;
+
 namespace MonguGamerPro
 {
     public partial class Form1 : Form
     {
+
+        public const byte VK_NUMLOCK = 0x90;
+        public const byte VK_CAPSLOCK = 0x14;
+        public const byte VK_SCROLL = 0x91;
+
+        public const byte VK_F13 = 0x7C;
+        public const byte VK_F14 = 0x7D;
+        public const byte VK_F15 = 0x7E;
+        public const byte VK_F16 = 0x7F;
+        public const byte VK_F17 = 0x80;
+        public const byte VK_F18 = 0x81;
+        public const byte VK_F19 = 0x82;
+        public const byte VK_F20 = 0x83;
+        public const byte VK_F21 = 0x84;
+        public const byte VK_F22 = 0x85;
+        public const byte VK_F23 = 0x86;
+        public const byte VK_F24 = 0x87;
+
+        [DllImport("user32.dll")]
+        static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
+        const int KEYEVENTF_EXTENDEDKEY = 0x1;
+        const int KEYEVENTF_KEYUP = 0x2;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void numLockButton_Click(object sender, EventArgs e)
+        {
+            if (Control.IsKeyLocked(Keys.NumLock))
+            {
+                keybd_event(VK_NUMLOCK, 0x45, KEYEVENTF_EXTENDEDKEY, (UIntPtr)0);
+                keybd_event(VK_NUMLOCK, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, (UIntPtr)0);
+
+                numLockLabel.Text = "OFF";
+                numLockLabel.BackColor = Color.Red;
+            }
+            else
+            {
+                numLockLabel.Text = "ON";
+                numLockLabel.BackColor = Color.Green;
+            }
+        }
+
+        private void capsLockButton_Click(object sender, EventArgs e)
+        {
+            if (Control.IsKeyLocked(Keys.CapsLock))
+            {
+                keybd_event(VK_CAPSLOCK, 0x45, KEYEVENTF_EXTENDEDKEY, (UIntPtr)0);
+                keybd_event(VK_CAPSLOCK, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, (UIntPtr)0);
+
+                capsLockLabel.Text = "OFF";
+                capsLockLabel.BackColor = Color.Red;
+            }
+            else
+            {
+                keybd_event(VK_CAPSLOCK, 0x45, 0, (UIntPtr)0);
+
+                capsLockLabel.Text = "ON";
+                capsLockLabel.BackColor = Color.Green;
+            }
+        }
+
+        private void scrolLockButton_Click(object sender, EventArgs e)
+        {
+            if (Control.IsKeyLocked(Keys.Scroll))
+            {
+                keybd_event(VK_SCROLL, 0x45, KEYEVENTF_EXTENDEDKEY, (UIntPtr)0);
+                keybd_event(VK_SCROLL, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, (UIntPtr)0);
+
+                scrollLockLabel.Text = "OFF";
+                scrollLockLabel.BackColor = Color.Red;
+            }
+            else
+            {
+                scrollLockLabel.Text = "ON";
+                scrollLockLabel.BackColor = Color.Green;
+            }
         }
     }
 }
